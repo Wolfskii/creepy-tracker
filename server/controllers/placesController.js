81edf79ef89384a965c97b3d9997960b62b142b2
Dawid Kaleta <dawid.kaleta@outlook.com>
2022-01-07 22:08:15 +0100

    Implementing connecting to MongoDB and retrieval of collections items

--- server/controllers/placesController.js
+++ server/controllers/placesController.js
@@ -1,4 +1,3 @@
-const axios = require('axios')
 const db = require('../dbConnection')
 
 const placesController = {}
@@ -8,9 +7,6 @@ placesController.getPlaces = async (req, res, next) => {
     await db.connect()
     const items = await db.getAll('places')
     await db.disconnect()
-
-    // const response = await axios.get('/')
-    // const jsonData = await response.json()
 
     res.status(200).json(items)
   } catch (error) {
